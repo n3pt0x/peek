7356d88b02d43a179edc9093d277bc13a0669fc5
n3pt0x <157751271+n3pt0x@users.noreply.github.com>
2026-09-24 23:18:53 +0200

    update: struct StatusPort + enum PortState

--- src/protocols/common.h
+++ src/protocols/common.h
@@ -4,6 +4,18 @@
 #include <stdint.h>
 #include <stddef.h>
 
+typedef enum {
+    PORT_UNKNOWN,
+    PORT_OPEN,
+    PORT_CLOSED,
+    PORT_FILTERED
+} PortState;
+
+typedef struct StatusPort {
+    uint16_t port;
+    PortState status;
+} StatusPort;
+
 uint16_t checksum(const uint8_t *data, size_t data_len);
 void generate_random_data(void *packet, size_t packet_len);
 
